32f1a27475913dcc9d779891e1b7c326028bcc24
Francois Chaumont <funcois@outlook.com>
2018-12-23 14:03:11 -0600

    minor update and fix

--- example.php
+++ example.php
@@ -6,18 +6,15 @@ require __DIR__ . "/vendor/autoload.php";
 
 const CONFIG_FILE = __DIR__ . "/config.php";
 const CONFIG_DEBUG = __DIR__ . "/config.debug.php";
+const PARAMETERS_FILE = __DIR__ . "/parameters.php";
 
 require __DIR__ . '/usage.php';
 
 try {
     // NOTE: initializations
     // retrieve configuration data
-    if (is_readable(CONFIG_DEBUG)) {
-        $config = new \Noodlehaus\Config([CONFIG_FILE, CONFIG_DEBUG]);
-    } else {
-        $config = new \Noodlehaus\Config(CONFIG_FILE);
-    }
-    
+    $config = new \Noodlehaus\Config([CONFIG_FILE, '?' . CONFIG_DEBUG]);
+
     // init PHP error configuration
     ini_set("display_errors", $config->get('display-errors'));
     ini_set("error_reporting", $config->get('error-level'));
@@ -33,7 +30,7 @@ try {
 
     // init logger [optional]
     $logger = \FC\Logger\Log::getLogger(
-        $config->get('log-channel-name'), 
+        $config->get('log-channel-name'),
         $config->get('log-file-path'),
         $config->get('log-error-level'),
         $config->get('log-date-format'),
@@ -80,9 +77,11 @@ try {
     echo $e->getMessage() . "\n";
 
     // send notification
-    $notifications[] = $e->getMessage();
-    $text = $tempFolder . "/$outputFileName" . "\n\n" . implode("\n", $notifications);
-    PushOver::pushNotification($receiver, $subject, $text);
+    if (isset($receiver) && \FC\Helpers\ValidationHelper::checkEmailFormat($receiver)) {
+        $notifications[] = $e->getMessage();
+        $text = $tempFolder . "/$outputFileName" . "\n\n" . implode("\n", $notifications);
+        PushOver::pushNotification($receiver, $subject, $text);
+    }
 }
 
 echo "\nDone.\n";
